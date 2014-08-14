be5ea195d68fd75223e02fdc4d5c5e246ac132de
Danielle Graham <danielle.graham@mosaic.com>
2014-08-14 16:28:56 -0400

    add actual btns

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,3 @@
-// $(function(){
-//   var shrinkHeader = 200;
-//   $(window).scroll(function() {
-//     var scroll = getCurrentScroll();
-//     if ( scroll >= shrinkHeader ) {
-//       $("nav").addClass("scrolled");
-//     }
-//     else {
-//       $("nav").removeClass("scrolled");
-//     }
-//   });
-//   function getCurrentScroll() {
-//     return window.pageYOffset;
-//   }
-// });
-
 $('.bio-btn').click(function() {
   if ($('.bio').hasClass("bio-long")) {
     $('.bio').removeClass("bio-long");
@@ -26,34 +10,25 @@ $('nav').waypoint('sticky', {
   offset: 100,
   stuckClass: 'scrolled',
   wrapper: '<div class="nav-wrapper" />',
-  // handler: function() {
-  //   $('nav > .logo > h1').addClass("animated fadeInDown");
-  //   $('#menu').toggleClass("animated fadeInDown");
-  // }
 });
-
-// $('#bio').waypoint({
-
-//   handler: function(direction) {
-//    if (direction == 'up') {
-//       $('nav > .logo > h1').removeClass("animated fadeInDown");
-//       $('#menu').removeClass("animated fadeInDown");
-//       $('nav > .logo > h1').addClass("animated fadeoutnDown");
-//       $('#menu').addClass("animated fadeoutDown");
-      
-//     }
-//   }
-// });
 
 $('.section-light').waypoint(function() {
     $('nav').toggleClass('nav-dark');
+}, {
+  offset: 80
 });
 
 $('.section-light').waypoint(function(direction) {
   $('nav').toggleClass('nav-dark');
 }, {
   offset: function() {
-    return -$(this).outerHeight();
+    return -$(this).outerHeight() + 80;
   }
+});
+
+$('.btn-up').waypoint(function() {
+  $('.btn-up').addClass('animated fadeInUp');
+}, {
+  offset: 'bottom-in-view'
 });
 
